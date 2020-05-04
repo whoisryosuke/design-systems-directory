adb686e506551400bd244695add10eb26b7975dc
Ryosuke <ryosuke.san.hana@gmail.com>
2020-05-04 10:43:55 -0700

    :lipstick: Styled to design spec

--- src/components/SectionHeading/SectionHeading.tsx
+++ src/components/SectionHeading/SectionHeading.tsx
@@ -1,31 +1,45 @@
 import * as React from 'react';
-import styled from "styled-components"
-import { Box, Heading } from 'rebass/styled-components'
+import { Box, Heading, Text } from 'rebass/styled-components'
 
-interface ISectionHeadingProps {
+interface SectionHeadingProps {
   className: string,
-  emoji: string,
   heading: string,
+  highlight?: string,
   subtitle?: string
 }
 
-const SectionHeading: React.FunctionComponent<ISectionHeadingProps> = ({className, emoji, heading, subtitle, ...props}) => {
-  return(
-  <Box className={className} {...props}>
-    <Heading variant="heading">{emoji && emoji} {heading}</Heading>
-    {subtitle && <Heading variant="subtitle" mt={2} pl={4} mb={0}>{subtitle}</Heading>}
-  </Box>
+const SectionHeading: React.FunctionComponent<SectionHeadingProps> = ({className, highlight,heading, subtitle, ...props}) => {
+  return (
+    <Box className={className} mb={4} {...props}>
+      <Heading variant="subtitle" as="h2">
+        {heading}
+        {highlight && (
+          <Text
+            display="inline-block"
+            color="primary"
+            ml={2}
+            pb={1}
+            sx={{ borderBottom: '3px solid', borderColor: 'primary' }}
+          >
+            {highlight}
+          </Text>
+        )}
+      </Heading>
+      {subtitle && (
+        <Heading
+          as="h3"
+          color="gray.dark"
+          fontSize={[1, 2]}
+          fontWeight="normal"
+          mt={2}
+          mb={0}
+        >
+          {subtitle}
+        </Heading>
+      )}
+    </Box>
   )
 };
 
 
-const StyledSectionHeading = styled(SectionHeading)`
-  padding:2rem 1rem;
-  border-bottom:1px solid ${(props) => props.theme.colors.black};
-
-  ${props => props.tall && `
-    padding-top:7rem;
-  `}
-`
-
-export default StyledSectionHeading;
+export default SectionHeading
