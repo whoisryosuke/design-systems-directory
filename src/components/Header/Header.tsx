13a2d67dc0e2d8cf707a9a1ea554979201f1dc56
Ryosuke <ryosuke.san.hana@gmail.com>
2020-05-04 15:43:04 -0700

    :lipstick: Hamburger

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,49 +4,64 @@ import Link from 'gatsby-link'
 import { Box, Flex, Text } from 'rebass/styled-components'
 
 const MobileButton = styled(Box)`
-  .MobileButton {
-    margin-right: 2em;
+    margin-left: 2em;
 
-    & svg {
-      cursor: pointer;
-      -webkit-tap-highlight-color: transparent;
-      transition: transform ${props => props.theme.animation.default};
-      -moz-user-select: none;
-      -webkit-user-select: none;
-      -ms-user-select: none;
-      user-select: none;
+    width: 30px;
+    height: 26px;
+    position: relative;
+    transform: rotate(0deg);
+    transition: .5s ease-in-out;
+    cursor: pointer;
+    
+    & span {
+      display: block;
+      position: absolute;
+      height: 4px;
+      width: 100%;
+      background: ${({theme}) => theme.colors.black};;
+      border-radius: 9px;
+      opacity: 1;
+      left: 0;
+      transform: rotate(0deg);
+      transition: .25s ease-in-out;
     }
 
-    & .line {
-      fill: none;
-      transition: stroke-dasharray ${props => props.theme.animation.default},
-        stroke-dashoffset ${props => props.theme.animation.default};
-      stroke: #000;
-      stroke-width: 5.5;
-      stroke-linecap: round;
+    & span:nth-child(1) {
+      top: 0px;
+      transform-origin: left center;
     }
 
-    & svg .top {
-      stroke-dasharray: 40 130;
+    & span:nth-child(2) {
+      top: 11px;
+      transform-origin: left center;
     }
-    & svg .middle {
-      stroke-dasharray: 40 140;
+
+    & span:nth-child(3) {
+      top: 22px;
+      transform-origin: left center;
     }
-    & svg .bottom {
-      stroke-dasharray: 40 205;
-    }
-    & svg.active .top {
-      stroke-dasharray: 75 130;
-      stroke-dashoffset: -63px;
-    }
-    & svg.active .middle {
-      stroke-dashoffset: -102px;
-    }
-    & svg.active .bottom {
-      stroke-dasharray: 110 205;
-      stroke-dashoffset: -86px;
-    }
-  }
+
+    ${({ visible }) => visible && `
+      & span:nth-child(1) {
+        transform: rotate(45deg);
+        top: -1px;
+        left: 8px;
+      }
+
+      & span:nth-child(2) {
+        width: 0%;
+        opacity: 0;
+      }
+
+      & span:nth-child(3) {
+        -webkit-transform: rotate(-45deg);
+        -moz-transform: rotate(-45deg);
+        -o-transform: rotate(-45deg);
+        transform: rotate(-45deg);
+        top: 20px;
+        left: 8px;
+      }
+    `};
 `
 
 const StyledHeaderNav = styled.nav`
@@ -97,36 +112,19 @@ const StyledHeaderNav = styled.nav`
 
     & li { 
       width:100%;
+      margin:0;
       display:block;
       border-bottom:1px solid ${props.theme.colors.black};
 
       & a {
-        padding:4em 1em;
+        padding:3em 1em;
         transition:color 300ms ease-out;
 
         &:hover, &:focus {
+          background-color:${props.theme.colors.primary};
           color:${props.theme.colors.white};
         }
 
-        &:after {
-          content:'';
-          width:100%;
-          padding:4.5em 1em;
-          display:block;
-          position:absolute;
-          top: 0;
-          left: 0;
-          background:${props.theme.colors.primary};
-          border:0;
-          transform-origin: 0 50%;
-          transform: translate(-1.25em, 0) scaleX(0);
-          z-index: -1; 
-          transition:transform 300ms ease-out;
-        }
-
-        &:hover:after, &:focus:after  {
-          transform: translate(-1.25em, 0) scaleX(1);
-        }
       }
     }
   `}
@@ -189,23 +187,12 @@ const Header = React.memo(({mobile, visible, toggleVisibility}) => {
         {mobile && (
           <MobileButton
             type="button"
-            className="MobileButton"
+            visible={visible}
             onClick={toggleVisibility}
           >
-            <svg className={visible && 'active'} viewBox="0 0 100 100" width="50">
-              <path
-                className="line top"
-                d="m 70,33 h -40 c -11.092231,0 11.883874,13.496726 -3.420361,12.956839 -0.962502,-2.089471 -2.222071,-3.282996 -4.545687,-3.282996 -2.323616,0 -5.113897,2.622752 -5.113897,7.071068 0,4.448316 2.080609,7.007933 5.555839,7.007933 2.401943,0 2.96769,-1.283974 4.166879,-3.282995 2.209342,0.273823 4.031294,1.642466 5.857227,-0.252538 v -13.005715 16.288404 h 7.653568"
-              />
-              <path
-                className="line middle"
-                d="m 70,50 h -40 c -5.6862,0 -8.534259,5.373483 -8.534259,11.551069 0,7.187738 3.499166,10.922274 13.131984,10.922274 11.021777,0 7.022787,-15.773343 15.531095,-15.773343 3.268142,0 5.177031,-2.159429 5.177031,-6.7 0,-4.540571 -1.766442,-7.33533 -5.087851,-7.326157 -3.321409,0.0092 -5.771288,2.789632 -5.771288,7.326157 0,4.536525 2.478983,6.805271 5.771288,6.7"
-              />
-              <path
-                className="line bottom"
-                d="m 70,67 h -40 c 0,0 -3.680675,0.737051 -3.660714,-3.517857 0.02541,-5.415597 3.391687,-10.357143 10.982142,-10.357143 4.048418,0 17.88928,0.178572 23.482143,0.178572 0,2.563604 2.451177,3.403635 4.642857,3.392857 2.19168,-0.01078 4.373905,-1.369814 4.375,-3.392857 0.0011,-2.023043 -1.924401,-2.589191 -4.553571,-4.107143 -2.62917,-1.517952 -4.196429,-1.799562 -4.196429,-3.660714 0,-1.861153 2.442181,-3.118811 4.196429,-3.035715 1.754248,0.0831 4.375,0.890841 4.375,3.125 2.628634,0 6.160714,0.267857 6.160714,0.267857 l -0.178571,-2.946428 10.178571,0 -10.178571,0 v 6.696428 l 8.928571,0 -8.928571,0 v 7.142858 l 10.178571,0 -10.178571,0"
-              />
-            </svg>
+            <span></span>
+            <span></span>
+            <span></span>
           </MobileButton>
         )}
       </Box>
