499ff5fc26ef806873d6d747823279017dbe5184
Ryosuke <ryosuke.san.hana@gmail.com>
2020-05-04 17:15:55 -0700

    :bug: Mobile fix

--- src/components/Masthead/Masthead.tsx
+++ src/components/Masthead/Masthead.tsx
@@ -12,8 +12,8 @@ const Masthead: React.FunctionComponent<MastheadProps> = ({header, subheader}) =
       <Heading fontSize={[5, 6, 7]} mb={3}>{header}</Heading>
       <Heading fontSize={[1, 2, 3]} fontWeight="normal" mb={4}>{subheader}</Heading>
       <Flex width="80%" mx="auto" justifyContent="center" mb={3} flexDirection={['column', 'row']}>
-        <Button width={[1, 1 / 2]} variant="masthead" mr={[0, 3]} mb={[3, 0]}>Browse systems</Button>
-        <Button width={[1, 1 / 2]} variant="masthead" color="black" bg="white" sx={{
+        <Button variant="masthead" mr={[0, 3]} mb={[3, 0]}>Browse systems</Button>
+        <Button variant="masthead" color="black" bg="white" sx={{
           ':hover,:focus,.active': {
             color: 'black',
             bg: 'white',
